bcc5def8c2921fde6c62da2001ddebc693eaeeed
DocGandalf <kody@vitalism.pro>
2016-07-20 13:55:30 -0600

    Deleted Editor
    And everything related to it.

--- Work/src/master/Master.java
+++ Work/src/master/Master.java
@@ -1,10 +1,8 @@
 package master;
 
 import java.awt.*;
-import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.event.*;
-import java.util.*;
 import java.io.*;
 
 @SuppressWarnings("serial")
@@ -13,16 +11,10 @@ public class Master extends JFrame{
 	@SuppressWarnings("rawtypes")
 	private JList diagnoses;
 	private JScrollPane scrollDiagnoses;
-	private JButton newDiagnosis=new JButton("New");
 	private JButton addPatientInfo=new JButton("Go");
-	private JButton editDiagnosis=new JButton("Edit");
-	private Master instance;
-	private EditViewer eView;
-	private Editor editor;
 	
 	@SuppressWarnings({ "unchecked", "rawtypes" })
 	public Master(){
-		instance=this;
 		for(int i=0; i<diagnosesNames.length; i++){
 			diagnosesNames[i]=diagnosesNames[i].substring(0, diagnosesNames[i].indexOf('.'));
 		}
@@ -33,7 +25,6 @@ public class Master extends JFrame{
 			public void valueChanged(ListSelectionEvent e){
 				int[] indices=diagnoses.getSelectedIndices();
 				addPatientInfo.setEnabled(indices.length!=0);
-				editDiagnosis.setEnabled(indices.length==1);
 			}
 		});
 		
@@ -41,41 +32,9 @@ public class Master extends JFrame{
 		add(scrollDiagnoses, BorderLayout.SOUTH);
 		
 		addPatientInfo.setEnabled(false);
-		editDiagnosis.setEnabled(false);
-		
-		editDiagnosis.addActionListener(new ActionListener(){
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				instance.setVisible(false);
-				eView=new EditViewer(instance, new String[]{(String)diagnoses.getSelectedValue()});
-				editor=new Editor(instance);
-			}
-		});
-		newDiagnosis.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				instance.setVisible(false);
-				eView=new EditViewer(instance);
-				editor=new Editor(instance);
-			}
-		});
-		
-		JPanel buttonPanel=new JPanel();
-		buttonPanel.setLayout(new GridLayout(1,3));
-		buttonPanel.add(addPatientInfo);
-		buttonPanel.add(newDiagnosis);
-		buttonPanel.add(editDiagnosis);
-		add(buttonPanel, BorderLayout.CENTER);
+		add(addPatientInfo, BorderLayout.CENTER);
 		
 		setPreferredSize(new Dimension(250,192));
-	}
-	
-	public EditViewer getEditViewer(){
-		return eView;
-	}
-	
-	public Editor getEditor(){
-		return editor;
 	}
 	
 	public static void main(String[] args){
